c0b975db89ff74ec99a8365c34785ed60a15dc26
slayne0 <majfeuguicheteau@gmail.com>
2025-02-19 15:48:50 +0100

    fonctionel

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const $gameScreenGridCells = document.querySelectorAll(".grid")
 // const $myCell = document.querySelector('.grid-case[data-x="2"][data-y="2"]')
 // $myCell.classList.add("tomato")
 
+let yelloWin = false
+let redWin = false 
 let currentPlayer = "y"
 let notplayer = "r"
 let gameBoard = [
@@ -24,37 +26,174 @@ $grid.forEach(function($gameScreenGridCell) {
 
 
 
-function checkWin(board, y, x) {
+function checkWin(board, y, DX) {
+       // colonne
     let result = 0
     let player = notplayer
+    let  = 1
+    let passage = 0
+    let x = parseInt(DX)
 
     
-    // colonne
-    for (let i = 0; i < 4; i++) {
-        // 2. alors, tu vas ici
-        if (y - number < 0) {
-            number = i
-            for (let i = 1; i < 6-y; i++) {
-                if (board[y+i][x] === player) {
-                    result += 1
-                } else {
-                    if (result >= 4) {
-                        return true
-                    } else {
-                        return false
-                    }
-                }
+
+    for (let e = 0; e < 5; e++) {
+        if (result >= 4) {
+            if (player === "y") {
+                yelloWin = true
+            } else {
+                redWin = true 
             }
-        } else {
-            if (isValid) {
-                // 1. Si tu ne vas ici
+        } 
+        if (passage === 0 && y > 0 ) {
+            if (board[y-e][x] === player) {
                 result += 1
             } else {
-                number = 30
+                passage = 1
+                e = 0
             }
+        } else {  
+            if (e+y > 5) {
+            }  else {         
+                
+                if (board[y+e][x] === player) {
+                    result += 1
+                } else {
+                    break
+                }
+            }
+                
+            
+        }
+    }
+
+
+
+
+
+   // ligne
+    result = 0
+    player = notplayer
+    o = 1
+    passage = 0
+
+    
+
+    for (let e = 0; e < 5; e++) {
+        if (result >= 4) {
+            if (player === "y") {
+                yelloWin = true
+            } else {
+                redWin = true 
+            }
+        } 
+        if (passage === 0 && y > 0 ) {
+            if (board[y][x-e] === player) {
+                result += 1
+            } else {
+                passage = 1
+                e = 0
+            }
+        } else {  
+            if (e+y > 5) {
+            }  else {         
+                
+                if (board[y][x+e] === player) {
+                    result += 1
+                } else {
+                    break
+                }
+            }
+                
+            
+        }
+    }
+
+
+
+
+   // diagonale haut gauche 
+    result = 0
+    player = notplayer
+    o = 1
+    passage = 0
+
+    
+
+    for (let e = 0; e < 5; e++) {
+        console.log("====")
+        if (result >= 4) {
+            if (player === "y") {
+                yelloWin = true
+            } else {
+                redWin = true 
+            }
+        } 
+        if (passage === 0 && y > 0 ) {
+            if (board[y-e][x-e] === player) {
+                result += 1
+            } else {
+                passage = 1
+                e = -1
+            }
+        } else {  
+            if (e+y > 5 || e === -1) {
+            }  else {         
+                if (board[y+e][x+e] === player) {
+                    result += 1
+                    console.log(result)
+                } else {
+                }
+            }
+                
+            
+        }
+    }
+
+
+
+
+   // diagonale haut droite 
+    result = 0
+    lplayer = notplayer
+    o = 1
+    passage = 0
+
+    
+    for (let e = 0; e < 5; e++) {
+        if (result >= 4) {
+            if (player === "y") {
+                yelloWin = true
+            } else {
+                redWin = true 
+            }
+        } 
+        if (passage === 0 && y > 0 ) {
+            if (board[y-e][x+e] === player) {
+                result += 1
+            } else {
+                passage = 1
+                e = 0
+            }
+        } else {  
+            if (e+y > 5) {
+            }  else {         
+                
+                if (board[y+e][x-e] === player) {
+                    result += 1
+                } else {
+                    break
+                }
+            }
+                
+            
         }
     }
 }
+
+
+
+
+
 
 
 
@@ -67,6 +206,9 @@ $grid.forEach(function($grid) {
         const dataX = $grid.getAttribute("data-x")
         const dataY = $grid.getAttribute("data-y") 
 
+        const cDeX = $grid.getAttribute(parseInt("data-x"))
+        // console.log(parseInt(dataX))
+
 
 
 
@@ -74,7 +216,6 @@ $grid.forEach(function($grid) {
 
         for (let i = 5; i >= 0; i--) {
             const $test = document.querySelector(`.grid-case[data-x="${dataX}"][data-y="${i}"]`)
-            const isWin = checkWin(gameBoard, i, dataX)
 
 
             if (gameBoard[i][dataX] === "y" || gameBoard[i][dataX] === "r") {
@@ -84,21 +225,22 @@ $grid.forEach(function($grid) {
                     $test.classList.add("yellow")
                     currentPlayer = "r"
                     notplayer = "y"
+                    const isWin = checkWin(gameBoard, i, dataX)
                     return
                 } else {
                     gameBoard[i][dataX] = currentPlayer
                     $test.classList.add("red")
                     currentPlayer = "y"
                     notplayer = "r"
+                    const isWin = checkWin(gameBoard, i, dataX)
                     return
                 }
+                
 
                 // console.log(isWin(gameBoard, i, dataX))
 
 
-                // if (isWin) {
-                //     console("abelibacenfly")
-                // }
+
 
                 
                 
